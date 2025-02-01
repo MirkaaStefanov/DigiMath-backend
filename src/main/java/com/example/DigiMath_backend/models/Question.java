e10938195c0769fc956e9986015105f041e404d7
MirkaanStefanov <135588213+MirkaaStefanov@users.noreply.github.com>
2025-02-01 14:14:17 +0200

    Create models and their repositories; Test,Question,Answer

---
+++ src/main/java/com/example/DigiMath_backend/models/Question.java
@@ -0,0 +1,48 @@
+package com.example.DigiMath_backend.models;
+
+
+import com.example.DigiMath_backend.enums.QuestionType;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "questions")
+public class Question {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "test_id")
+    private Test test;
+
+    private String text;
+
+    @Enumerated(EnumType.STRING)
+    private QuestionType questionType;
+
+    @OneToMany(mappedBy = "question", cascade = CascadeType.ALL)
+    private List<Answer> answers;
+
+
+}

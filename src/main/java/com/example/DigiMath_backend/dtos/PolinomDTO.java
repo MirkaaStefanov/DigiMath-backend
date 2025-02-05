4ea8cbdec746290f104de2b2af92cdbb0942dc20
MirkaanStefanov <135588213+MirkaaStefanov@users.noreply.github.com>
2025-02-05 18:12:19 +0200

    Intigrate polinom

---
+++ src/main/java/com/example/DigiMath_backend/dtos/PolinomDTO.java
@@ -0,0 +1,25 @@
+package com.example.DigiMath_backend.dtos;
+
+import jakarta.persistence.CollectionTable;
+import jakarta.persistence.Column;
+import jakarta.persistence.ElementCollection;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class PolinomDTO {
+
+    private Long id;
+    private int degree;
+    private List<Double> coefficients;
+
+}
